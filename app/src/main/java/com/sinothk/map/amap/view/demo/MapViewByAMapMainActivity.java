c0b0a872817aabbad0d0fe9faac2f4c9611dd092
LiangYT <381518188@qq.com>
2019-01-16 10:36:16 +0800

    同步

---
+++ app/src/main/java/com/sinothk/map/amap/view/demo/MapViewByAMapMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.map.amap.view.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapViewByAMapMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view_by_amap_main);
+    }
+}
